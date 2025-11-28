cdd2aa2ce98b14166a588607cffe870385b3ec5f
Marvellous <akomolafemarvellous@gmail.com>
2025-11-28 02:14:12 +0100

    feat: Implement Single Page Application support and update routing configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import { AnimatePresence, motion } from 'framer-motion'
 import { ThemeProvider } from './contexts/ThemeContext'
 import Layout from './components/Layout'
